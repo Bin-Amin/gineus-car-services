c55955fb3a7aa2f15305b04afa59a59ec26bb19f
Bin-Amin <hmsolaiman6@gmail.com>
2022-04-11 16:51:45 +0600

    image weight and w-auto add

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -5,11 +5,11 @@ const Service = ({ service }) => {
     const { name, img, description, price } = service;
     return (
         <div className="service">
-            <img src={img} alt="car repiar" />
+            <img className="w-100" src={img} alt="car repiar" />
             <h2>{name} </h2>
             <p>Price: ${price}</p>
             <p><small>{description}</small></p>
-            <button>Book: {name}</button>
+            <button className="btn btn-primary">Book: {name}</button>
         </div>
     );
 };
